af10b98e8dde915b985b731e5825810a598ae33e
Morakot Pilouk <mpilouk@esri.com>
2016-11-29 16:50:39 -0800

    Upgraded to 10.5.0
    Upgraded to 10.5.0

--- httpHandler-processor/src/main/java/com/esri/geoevent/processor/httpHandler/HttpHandlerDefinition.java
+++ httpHandler-processor/src/main/java/com/esri/geoevent/processor/httpHandler/HttpHandlerDefinition.java
@@ -104,7 +104,7 @@ public class HttpHandlerDefinition extends GeoEventProcessorDefinitionBase
   @Override
   public String getVersion()
   {
-    return "10.4.0";
+    return "10.5.0";
   }
 
   @Override
